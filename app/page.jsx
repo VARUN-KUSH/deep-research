7526b4df7ec2308d1844f537cac45732ad0df00f
varun kumar <varunkumar@Mac.lan>
2025-07-11 21:07:08 +0530

    improved prompt

--- app/page.jsx
+++ app/page.jsx
@@ -17,7 +17,6 @@ const App = () => {
     const [isLoading, setIsLoading] = useState(false); // Indicates if an API call is in progress
     const [message, setMessage] = useState(''); // Displays user feedback messages (success/error)
 
-
     const NEXT_PUBLIC_OPENAI_API_KEY="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"
     const openai = new OpenAI({
         apiKey: NEXT_PUBLIC_OPENAI_API_KEY, dangerouslyAllowBrowser: true // Use environment variable for OpenAI API key
